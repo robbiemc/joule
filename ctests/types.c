b694a9e13c0dc4544599ef0e577dbceb567515c8
Alex Crichton <alex@alexcrichton.com>
2012-01-25 13:15:16 -0500

    Exercise lv_gettype in the types test

--- ctests/types.c
+++ ctests/types.c
@@ -12,17 +12,21 @@ int main() {
   /** NUMBERS **/
   luav one = lv_number(1);
   assert(same(lv_cvt(one), 1.0));
+  assert(lv_gettype(one) == LNUMBER);
 
   luav two = lv_number(2);
   assert(same(lv_getnumber(two), 2.0));
+  assert(lv_gettype(two) == LNUMBER);
 
   luav nan = lv_number(NAN);
   assert(lv_bits(lv_getnumber(nan)) == lv_bits(NAN));
   assert(isnan(lv_getnumber(nan)));
+  assert(lv_gettype(nan) == LNUMBER);
 
   luav inf = lv_number(1.0/0.0);
   assert(lv_bits(lv_getnumber(inf)) == lv_bits(1.0/0.0));
   assert(isinf(lv_getnumber(inf)));
+  assert(lv_gettype(inf) == LNUMBER);
 
   assert(lv_hash(one) != lv_hash(two));
 
@@ -33,11 +37,14 @@ int main() {
   assert(isnan(lv_cvt(b_true1)));
   assert(isnan(lv_cvt(b_true2)));
   assert(lv_getbool(b_true1) == 1);
+  assert(lv_gettype(b_true1) == LBOOLEAN);
+  assert(lv_gettype(b_true2) == LBOOLEAN);
 
   luav b_false = lv_bool(0);
   assert(b_true1 != b_false);
   assert(isnan(lv_cvt(b_false)));
   assert(lv_getbool(b_false) == 0);
+  assert(lv_gettype(b_false) == LBOOLEAN);
 
   assert(lv_hash(b_true1) != lv_hash(b_false));
   assert(lv_hash(b_true1) == lv_hash(b_true2));
@@ -45,6 +52,7 @@ int main() {
   /** Nil **/
   luav b_nil = LUAV_NIL;
   assert(isnan(lv_cvt(b_nil)));
+  assert(lv_gettype(b_nil) == LNIL);
 
   /** User data **/
   luav udata = lv_userdata((void*) 0x1234);
@@ -54,6 +62,8 @@ int main() {
   assert(isnan(lv_cvt(udata2)));
   assert(lv_getuserdata(udata2) == (void*) 0x1235);
   assert(udata != udata2);
+  assert(lv_gettype(udata) == LUSERDATA);
+  assert(lv_gettype(udata2) == LUSERDATA);
 
   /** Tables **/
   lhash_t table1, table2;
@@ -63,6 +73,8 @@ int main() {
   assert(ltable1 != ltable2);
   assert(lv_gettable(ltable1) == &table1);
   assert(lv_gettable(ltable2) == &table2);
+  assert(lv_gettype(ltable1) == LTABLE);
+  assert(lv_gettype(ltable2) == LTABLE);
 
   return 0;
 }
