f6c1e0e607063b0ac4b4f13d1059c0f8d0b3694d
Alex Crichton <alex@alexcrichton.com>
2012-01-30 02:13:43 -0500

    Implement os.date(), and add printf to the tests

--- tests/printf.lua
+++ tests/printf.lua
@@ -4,4 +4,4 @@ function printf(...)
  io.write(string.format(...))
 end
 
-printf("Hello %s from %s on %s\n",os.getenv"USER" or "there",_VERSION,os.date())
+printf("Hello %s from %s on %s\n",os.getenv"USER" or "there",'lua',os.date())
