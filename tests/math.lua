e10084a128133baea5863edac7534814ed6bee97
Alex Crichton <alex@alexcrichton.com>
2012-01-29 21:11:47 -0500

    Add some more cruft to math.lua

--- tests/math.lua
+++ tests/math.lua
@@ -19,3 +19,21 @@ print('0x20' + 1)
 
 print(tonumber(' 10 '))
 print(-'2')
+
+function f(...)
+  if select('#', ...) == 1 then
+    return (...)
+  else
+    return "***"
+  end
+end
+
+print(f(tonumber('1  a')))
+assert(f(tonumber('e1')) == nil)
+assert(f(tonumber('e  1')) == nil)
+assert(f(tonumber(' 3.4.5 ')) == nil)
+print(tonumber(''))
+assert(f(tonumber('', 8)) == nil)
+assert(f(tonumber('  ')) == nil)
+assert(f(tonumber('  ', 9)) == nil)
+assert(f(tonumber('99', 8)) == nil)
