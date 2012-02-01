f0d4210e7d9bc4234cb9d13eaa588bfb7af1fa5b
Robbie McElrath <rmcelrath@cmu.edu>
2012-02-01 02:33:18 -0500

    Added a basic test to check the types of things passed to metamethods

---
+++ tests/metabasic.lua
@@ -0,0 +1,17 @@
+t = {}
+m = {}
+
+setmetatable(t, m)
+
+m.__add = function(a,b)
+  print(type(a), type(b))
+  return 2.7
+end
+
+print(t + 44)
+print(t + '4')
+print(t + t)
+print(40 + t)
+print('hi' + t)
+print('3.14' + t)
+print(t + true)
