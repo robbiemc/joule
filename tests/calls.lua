c76bff1e2bc45d4c6c07239da405182815ed28cc
Alex Crichton <alex@alexcrichton.com>
2012-02-06 15:13:33 -0500

    Add a new tests

--- tests/calls.lua
+++ tests/calls.lua
@@ -25,34 +25,34 @@ do
 end
 assert(fact == false)
 
--- -- testing declarations
--- a = {i = 10}
--- self = 20
--- function a:x (x) return x+self.i end
--- function a.y (x) return x+self end
---
--- assert(a:x(1)+10 == a.y(1))
---
--- a.t = {i=-100}
--- a["t"].x = function (self, a,b) return self.i+a+b end
---
--- assert(a.t:x(2,3) == -95)
---
--- do
---   local a = {x=0}
---   function a:add (x) self.x, a.y = self.x+x, 20; return self end
---   assert(a:add(10):add(20):add(30).x == 60 and a.y == 20)
--- end
---
--- local a = {b={c={}}}
---
--- function a.b.c.f1 (x) return x+1 end
--- function a.b.c:f2 (x,y) self[x] = y end
--- assert(a.b.c.f1(4) == 5)
--- a.b.c:f2('k', 12); assert(a.b.c.k == 12)
---
--- print('+')
---
+-- testing declarations
+a = {i = 10}
+self = 20
+function a:x (x) return x+self.i end
+function a.y (x) return x+self end
+
+assert(a:x(1)+10 == a.y(1))
+
+a.t = {i=-100}
+a["t"].x = function (self, a,b) return self.i+a+b end
+
+assert(a.t:x(2,3) == -95)
+
+do
+  local a = {x=0}
+  function a:add (x) self.x, a.y = self.x+x, 20; return self end
+  assert(a:add(10):add(20):add(30).x == 60 and a.y == 20)
+end
+
+local a = {b={c={}}}
+
+function a.b.c.f1 (x) return x+1 end
+function a.b.c:f2 (x,y) self[x] = y end
+assert(a.b.c.f1(4) == 5)
+a.b.c:f2('k', 12); assert(a.b.c.k == 12)
+
+print('+')
+
 t = nil   -- 'declare' t
 function f(a,b,c) local d = 'a'; t={a,b,c,d} end
 
@@ -102,8 +102,8 @@ deep(200)
 function deep (n) if n>0 then return deep(n-1) else return 101 end end
 assert(deep(30000) == 101)
 a = {}
--- function a:deep (n) if n>0 then return self:deep(n-1) else return 101 end end
--- assert(a:deep(30000) == 101)
+function a:deep (n) if n>0 then return self:deep(n-1) else return 101 end end
+assert(a:deep(30000) == 101)
 
 print('+')
 
