76161b5b91ae1d485c88c0acce6459107c9c983e
Robbie McElrath <rmcelrath@cmu.edu>
2012-02-07 00:24:35 -0500

    Added a couple io functions

--- src/lib/io.c
+++ src/lib/io.c
@@ -25,25 +25,33 @@
 static luav str_r;
 static luav str_w;
 static luav str_open_failed;
+static luav str_close_std;
 static lhash_t fd_meta;
 static FILE *default_out;
 static FILE *default_in;
 
 static lhash_t lua_io;
-static u32 lua_io_write(LSTATE);
-static u32 lua_io_open(LSTATE);
+static u32 lua_io_close(LSTATE);
+static u32 lua_io_flush(LSTATE);
 static u32 lua_io_input(LSTATE);
+static u32 lua_io_lines(LSTATE);
+static u32 lua_io_open(LSTATE);
+static u32 lua_io_write(LSTATE);
 static u32 lua_io_meta_index(LSTATE);
 
-static LUAF(lua_io_write);
-static LUAF(lua_io_open);
+static LUAF(lua_io_close);
+static LUAF(lua_io_flush);
 static LUAF(lua_io_input);
+static LUAF(lua_io_lines);
+static LUAF(lua_io_open);
+static LUAF(lua_io_write);
 static LUAF(lua_io_meta_index);
 
 INIT static void lua_io_init() {
   str_r = LSTR("r");
   str_w = LSTR("w");
-  str_open_failed = LSTR("Error opening file!");
+  str_open_failed = LSTR("Error opening file");
+  str_close_std = LSTR("cannot close standard file");
   default_out = stdout;
   default_in  = stdin;
 
@@ -53,15 +61,39 @@ INIT static void lua_io_init() {
                       lv_function(&lua_io_meta_index_f));
 
   lhash_init(&lua_io);
-  REGISTER(&lua_io, "write",  &lua_io_write_f);
-  REGISTER(&lua_io, "open",   &lua_io_open_f);
+  REGISTER(&lua_io, "close",  &lua_io_close_f);
+  REGISTER(&lua_io, "flush",  &lua_io_flush_f);
   REGISTER(&lua_io, "input",  &lua_io_input_f);
+  REGISTER(&lua_io, "lines",  &lua_io_lines_f);
+  REGISTER(&lua_io, "open",   &lua_io_open_f);
+  REGISTER(&lua_io, "write",  &lua_io_write_f);
 
   lhash_set(&lua_globals, LSTR("io"), lv_table(&lua_io));
 }
 
 DESTROY static void lua_io_destroy() {
   lhash_free(&lua_io);
+}
+
+static u32 lua_io_close(LSTATE) {
+  FILE *file = default_out;
+  if (argc > 0)
+    file = (FILE*) lstate_getuserdata(0);
+  if (file == stdout || file == stdin) {
+    lstate_return(LUAV_NIL, 0);
+    lstate_return(str_close_std, 1);
+    return 2;
+  }
+  fclose(file);
+  lstate_return1(LUAV_TRUE);
+}
+
+static u32 lua_io_flush(LSTATE) {
+  FILE *file = default_out;
+  if (argc > 0)
+    file = (FILE*) lstate_getuserdata(0);
+  fflush(file);
+  lstate_return1(LUAV_TRUE);
 }
 
 static u32 lua_io_input(LSTATE) {
@@ -84,6 +116,10 @@ static u32 lua_io_input(LSTATE) {
     }
   }
   lstate_return1(lv_userdata(default_in));
+}
+
+static u32 lua_io_lines(LSTATE) {
+  return 0;
 }
 
 static u32 lua_io_open(LSTATE) {
