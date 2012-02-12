5c15ce0ed13f2cbf8699b9e36e6a06dd1f44ab33
Alex Crichton <alex@alexcrichton.com>
2012-02-12 01:05:47 -0500

    More comments

--- src/vm.c
+++ src/vm.c
@@ -14,33 +14,31 @@
 #include "panic.h"
 #include "vm.h"
 
+/* Macros for dealing with the stack (accessing, setting) */
 #define STACKI(n) (stack + (n))
 #define STACK(n) vm_stack->base[STACKI(n)]
 #define CONST(n) ({ assert((n) < func->num_consts); func->consts[n]; })
 #define REG(n)                                                                \
   ({                                                                          \
-    assert(&STACK(n) < vm_stack->top);                                         \
+    assert(&STACK(n) < vm_stack->top);                                        \
     luav _tmp = STACK(n);                                                     \
     lv_isupvalue(_tmp) ? lv_getupvalue(_tmp)->value : _tmp;     \
   })
 #define SETREG(n, v)                            \
   ({                                            \
-    assert(&STACK(n) < vm_stack->top);           \
+    assert(&STACK(n) < vm_stack->top);          \
     if (lv_isupvalue(STACK(n))) { \
       lv_getupvalue(STACK(n))->value = v;       \
     } else {                                    \
       STACK(n) = v;                             \
     }                                           \
   })
-
-/* TODO: extract out 256 */
 #define KREG(n) ((n) >= 256 ? CONST((n) - 256) : REG(n))
 #define UPVALUE(closure, n)                                \
   ({                                                       \
     assert((n) < (closure)->function.lua->num_upvalues);   \
     (closure)->upvalues[n];                                \
   })
-#define DECODEFP8(v) (((u32)8 | ((v)&7)) << (((u32)(v)>>3) - 1))
 
 /* Metatable macros */
 #define BINOP_ADD(a,b) ((a)+(b))
@@ -82,12 +80,12 @@
           }                                                               \
         }
 
-lhash_t userdata_meta;
-lhash_t lua_globals;
-lhash_t *global_env = NULL;
-lframe_t *vm_running = NULL;
-lstack_t *vm_stack;
-static lstack_t init_stack;
+lhash_t userdata_meta;       //<! metatables for all existing userdata
+lhash_t lua_globals;         //<! default global environment
+lhash_t *global_env = NULL;  //<! current global environment
+lframe_t *vm_running = NULL; //<! currently running function's frame
+lstack_t *vm_stack;          //<! current stack, changes on thread switches
+static lstack_t init_stack;  //<! initial stack
 
 static void op_close(u32 upc, luav *upv);
 static int meta_unary(luav operand, u32 op, u32 reti, lframe_t *frame);
@@ -99,6 +97,11 @@ static void meta_lhash_set(luav operand, luav key, luav val, lframe_t *frame);
 static u32  meta_call(luav value, u32 argc, u32 argvi, u32 retc, u32 retvi);
 static luav meta_concat(luav v1, luav v2);
 
+/**
+ * @brief Initializes all global structures
+ *
+ * Requres that the strings have been initialized
+ */
 INIT static void vm_setup() {
   lhash_init(&userdata_meta);
   lhash_init(&lua_globals);
@@ -111,8 +114,15 @@ INIT static void vm_setup() {
 
 DESTROY static void vm_destroy() {
   lhash_free(&lua_globals);
+  lhash_free(&userdata_meta);
 }
 
+/**
+ * @brief Initialize a lua stack
+ *
+ * @param stack the stack to initialize
+ * @param size the initial size of the stack
+ */
 void vm_stack_init(lstack_t *stack, u32 size) {
   stack->size = 0;
   stack->limit = size;
@@ -120,6 +130,15 @@ void vm_stack_init(lstack_t *stack, u32 size) {
   stack->top = stack->base;
 }
 
+/**
+ * @brief Grow a stack by a given amount
+ *
+ * This should be called only if the current frame needs some more space, to
+ * grab a new frame, call vm_stack_alloc()
+ *
+ * @param stack the stack to grow
+ * @param amt the amount of stack slots to add
+ */
 void vm_stack_grow(lstack_t *stack, u32 amt) {
   stack->size += amt;
   if (stack->size >= stack->limit) {
@@ -129,12 +148,27 @@ void vm_stack_grow(lstack_t *stack, u32 amt) {
   stack->top = stack->base + stack->size;
 }
 
+/**
+ * @brief Allocate a new stack frame from the given stack
+ *
+ * @param stack the stack to allocate from
+ * @param amt the amount of stack to allocate
+ * @return the base of the stack, give this to vm_stack_dealloc when the stack
+ *         is done with being used
+ */
 u32 vm_stack_alloc(lstack_t *stack, u32 amt) {
   u32 base = stack->size;
   vm_stack_grow(stack, amt);
   return base;
 }
 
+/**
+ * @brief Deallocate the given stack to return the top of the stack to the given
+ *        base
+ *
+ * @param stack the stack to deallocate from
+ * @param base the return value from a previous call to vm_stack_alloc()
+ */
 void vm_stack_dealloc(lstack_t *stack, u32 base) {
   stack->size = base;
   if (stack->size > VM_STACK_INIT && stack->size < stack->limit / 2) {
@@ -144,30 +178,52 @@ void vm_stack_dealloc(lstack_t *stack, u32 base) {
   stack->top = stack->base + stack->size;
 }
 
+/**
+ * @brief Run a lua function, entry point into the VM
+ *
+ * @param func the function to run, typically from a parsed lua file.
+ */
 void vm_run(lfunc_t *func) {
+  /* set up the initial closure, then hit go */
   lclosure_t closure;
   closure.function.lua = func;
   closure.type = LUAF_LUA;
-  closure.env = &lua_globals;
-  global_env = &lua_globals;
+  closure.env  = &lua_globals;
+  global_env   = &lua_globals;
   assert(func->num_upvalues == 0);
+
   vm_fun(&closure, NULL, 0, 0, 0, 0);
 }
 
+/**
+ * @brief Run a function, dispatching based off the type of the function
+ *
+ * @param closure the closure to run
+ * @param parent the caller of this function, used to create stack traces
+ * @param LSTATE the lua state being invoked
+ */
 u32 vm_fun(lclosure_t *closure, lframe_t *parent, LSTATE) {
   lframe_t frame;
 
+  /* If we hit a TAILCALL, we don't want to continue to allocate stack, so
+     get the stack before the 'top:' label. Also, no need to get stack if we're
+     a C function */
   u32 stack = vm_stack->size;
   if (closure->type == LUAF_LUA) {
     stack = vm_stack_alloc(vm_stack, (u32) closure->function.lua->max_stack);
   }
+  /* TAILCALL works by packing all of the arguments into the current stack
+     frame, and then growing the stack for the next call frame. This means
+     that the stack to deallocate to can change, so keep a copy of the absolute
+     original bottom of the stack to deallocate to */
   u32 stack_orig = stack;
+  /* Entry point of TAILCALL */
 top:
   frame.caller  = parent;
   frame.closure = closure;
   vm_running    = &frame;
 
-  // handle c functions
+  /* handle c functions */
   if (closure->type != LUAF_LUA) {
     u32 ret = closure->function.c->f(argc, argvi, retc, retvi);
     vm_running = parent;
@@ -177,7 +233,6 @@ top:
     return ret;
   }
 
-  // it's a lua function
   u32 i, a, b, c, bx, limit;
   u32 last_ret = 0;
   lfunc_t *func = closure->function.lua;
@@ -186,6 +241,9 @@ top:
   luav temp;
   assert(closure->env != NULL);
 
+  /* Copy all arguments onto our stack, located in the lowest registers.
+     Might need to grow the stack if we're a VARARG function, and otherwise
+     might need to clip our number of parameters. */
   if (func->is_vararg) {
     if (argc > func->max_stack) {
       vm_stack_grow(vm_stack, argc - func->max_stack);
@@ -193,13 +251,14 @@ top:
   } else if (argc > func->num_parameters) {
     argc = func->num_parameters;
   }
-
   memcpy(&STACK(0), &vm_stack->base[argvi], sizeof(luav) * argc);
   assert(&STACK(argc) <= vm_stack->top);
   lv_nilify(&STACK(argc), vm_stack->size - argc - stack);
 
   /* Aligned memory accesses are much better */
   assert((((u64) instrs) & 3) == 0);
+
+  /* Core VM loop, also really slow VM loop... */
   while (1) {
     assert(instrs < func->instrs + func->num_instrs);
     u32 code = *instrs++;
@@ -213,7 +272,9 @@ top:
     }
 #endif
 
+    /* Dispatch based on what we're supposed to be doing */
     switch (OP(code)) {
+      /* R[A] = GLOBALS[CONST[BX]] */
       case OP_GETGLOBAL: {
         luav key = CONST(BX(code));
         assert(lv_isstring(key));
@@ -222,6 +283,7 @@ top:
         break;
       }
 
+      /* GLOBALS[CONST[BX]] = R[A] */
       case OP_SETGLOBAL: {
         luav key = CONST(BX(code));
         luav value = REG(A(code));
@@ -229,6 +291,7 @@ top:
         break;
       }
 
+      /* R[A] = R[B][R[C]] */
       case OP_GETTABLE: {
         luav table = REG(B(code));
         luav key = KREG(C(code));
@@ -236,6 +299,7 @@ top:
         break;
       }
 
+      /* R[A][R[B]] = R[C] */
       case OP_SETTABLE: {
         luav table = REG(A(code));
         luav key = KREG(B(code));
@@ -244,38 +308,51 @@ top:
         break;
       }
 
+      /* R[A] = UPVALUES[B], see OP_CLOSURE */
       case OP_GETUPVAL:
         temp = UPVALUE(closure, B(code));
         SETREG(A(code), lv_getupvalue(temp)->value);
         break;
 
+      /* UPVALUES[B] = R[A], see OP_CLOSURE */
       case OP_SETUPVAL:
         temp = UPVALUE(closure, B(code));
         lv_getupvalue(temp)->value = REG(A(code));
         break;
 
+      /* R[A] = CONST[BX] */
       case OP_LOADK:
         SETREG(A(code), CONST(BX(code)));
         break;
 
+      /* R[A..B] = nil */
       case OP_LOADNIL:
         for (i = A(code); i <= B(code); i++) {
           SETREG(i, LUAV_NIL);
         }
         break;
 
+      /* R[A] = R[B] */
       case OP_MOVE:
         SETREG(A(code), REG(B(code)));
         break;
 
+      /* Call a closure in a register, with a glob of parameters, receiving a
+         glob of return values */
       case OP_CALL: {
         a = A(code); b = B(code); c = C(code);
+        /* If we don't know how many arguments we're giving, then it's the last
+           number of arguments received from some previous instruction */
         u32 num_args = b == 0 ? last_ret - a - 1 : b - 1;
+        /* If we don't know how many return values we want, just say we want
+           everything ever, our stack will be grown for us by whomever is
+           returning value to us */
         u32 want_ret = c == 0 ? UINT_MAX : c - 1;
         u32 got;
         luav av = REG(a);
         lhash_t *meta = getmetatable(av);
 
+        /* Dispatch metatable __call if we can, otherwise recurse on vm_fun */
         if (meta != NULL) {
           got = meta_call(av, num_args, STACKI(a + 1),
                               want_ret, STACKI(a));
@@ -284,59 +361,94 @@ top:
           got = vm_fun(closure2, &frame, num_args, STACKI(a + 1),
                                              want_ret, STACKI(a));
         }
-        // fill in the nils
+        /* If we didn't get all the return values we wanted, then we need to
+           make sure we set all extra values to nil */
         for (i = got; i < c - 1 && &STACK(a + i) < vm_stack->top; i++) {
           SETREG(a + i, LUAV_NIL);
         }
+        /* Save how many things we just got, in case the next instruction
+           doesn't know how many things it wants */
         last_ret = a + got;
         break;
       }
 
+      /* Only exit point of the VM loop, returns a glob of parameters */
       case OP_RETURN:
         a = A(code);
         b = B(code);
-        if (b == 0) {
-          limit = last_ret - a;
-        } else {
-          limit = b - 1;
-        }
+        /* If we don't know what we're returning, then some previous instruction
+           received a glob of parameters and kept track of what it got */
+        limit = b == 0 ? last_ret - a : b - 1;
+        /* the RETURN opcode implicitly performs a CLOSE operation */
+        op_close(vm_stack->size - stack, vm_stack->base + stack);
+        /* TODO: does this need to grow the stack? */
         for (i = 0; i < limit && i < retc; i++) {
           vm_stack->base[retvi + i] = REG(a + i);
         }
-        op_close(vm_stack->size - stack, vm_stack->base + stack);
+        /* reset the currently running frame */
         vm_running = parent;
+        /* make sure we don't deallocate past the arguments returned */
         vm_stack_dealloc(vm_stack, MAX(retvi + i, stack_orig));
         return i;
 
+      /* Slightly optimized version of a CALL, but doesn't need any extra stack.
+         Implemented as a goto to the top of the VM loop, redoing all
+         intiaizliation.
+
+         Interesting part is preserving stacks. Arguments to the function need
+         to be on a separate portion of the stack than the running function's
+         stack frame, because it can call the VARARG instruction halfway in the
+         function to grab the arguments. To do this, all arguments to the
+         function are packed on the stack starting from stack_orig, and then
+         the new stack is allocated from the top of that stack. */
       case OP_TAILCALL: {
         a = A(code);
         b = B(code);
+        assert(C(code) == 0);
+        /* Figure out how big our glob is and where it's located */
         argc = (b == 0 ? last_ret : a + b) - a - 1;
         argvi = STACKI(a + 1);
         luav av = REG(a);
         lhash_t *meta = getmetatable(av);
+        /* As with CALL, dispatch the __call metamethod */
         if (meta != NULL) {
           /* TODO: bad error message? */
           closure = lv_getfunction(meta->metamethods[META_CALL], 0);
+          /* metamethod __call requires one extra argument, the table itself */
           vm_stack_grow(vm_stack, 1);
-          memmove(&vm_stack->base[stack + 1], &vm_stack->base[argvi],
+          memmove(&vm_stack->base[stack_orig + 1], &vm_stack->base[argvi],
                   argc * sizeof(luav));
-          vm_stack->base[stack] = av;
+          vm_stack->base[stack_orig] = av;
           argc++;
         } else {
           closure = lv_getfunction(av, 0);
-          memmove(&vm_stack->base[stack], &vm_stack->base[argvi],
+          memmove(&vm_stack->base[stack_orig], &vm_stack->base[argvi],
                   argc * sizeof(luav));
         }
-        argvi = stack;
+        argvi = stack_orig;
+        /* Bring the stack down to the top of our args */
+        vm_stack_dealloc(vm_stack, argvi + argc);
 
+        /* reallocate stack for lua functions */
         if (closure->type == LUAF_LUA) {
           stack = vm_stack_alloc(vm_stack, closure->function.lua->max_stack);
         }
-        assert(C(code) == 0);
         goto top;
       }
 
+      /* Create a lua function type, so it can be called. Possibly creates
+         upvalues for the function, which are variables that are defined outside
+         the scope of the function, but used inside the function.
+
+         Upvalues are implemented as a special type of luav that is a pointer
+         to another luav. These values are never exposed in lua, because they're
+         normally only stored in the upvalues array of closures. The only
+         difference is the fact that future references to the upvalue created
+         in the function that created the closure must update the upvalue, or
+         use the value that had been altered by a closure.
+
+         Hence, the values on the stack that are converted to upvalues are
+         replaced on the stack as an upvalue.*/
       case OP_CLOSURE: {
         bx = BX(code);
         assert(bx < func->num_funcs);
