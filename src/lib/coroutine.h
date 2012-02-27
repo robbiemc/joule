b077b5b6a405aa0d7e1b633db0b6849b57f2becf
Alex Crichton <alex@alexcrichton.com>
2012-02-26 21:28:22 -0500

    Commenting things in coroutine.h

--- src/lib/coroutine.h
+++ src/lib/coroutine.h
@@ -5,19 +5,21 @@
 #include "vm.h"
 
 typedef struct lthread {
-  int status;
-  void *stack;
+  int status;           //<! Current status of the thread
+  void *stack;          //<! Absolute bottom of the C-stack
 
+  /* Passing/returning arguments, all indices are relative to this thread's
+     stack located below */
   u32 argc;
   u32 argvi;
   u32 retc;
   u32 retvi;
 
-  struct lthread *caller;
-  lclosure_t *closure;
-  void *curstack;
-  lhash_t *env;
-  lstack_t vm_stack;
+  struct lthread *caller; //<! Thread who resumed us
+  lclosure_t *closure;    //<! Closure we are running
+  void *curstack;         //<! Last saved c-stack
+  lhash_t *env;           //<! Lua environment
+  lstack_t vm_stack;      //<! Lua stack
 } lthread_t;
 
 struct lthread* coroutine_current(void);
