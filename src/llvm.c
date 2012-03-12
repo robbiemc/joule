ec84e8adb91d6ec44b9a327c9030e5384d007933
Alex Crichton <alex@alexcrichton.com>
2012-03-12 03:19:36 -0500

    Add some initial compilation of functions, creating basic blocks, spaces for registers, and constants.
    All of this will probably change to something more reasonable in the future.

--- src/llvm.c
+++ src/llvm.c
@@ -15,6 +15,8 @@
 static LLVMModuleRef module;
 static LLVMPassManagerRef pass_manager;
 static LLVMExecutionEngineRef ex_engine;
+static LLVMBuilderRef builder;
+static LLVMTypeRef llvm_u64;
 
 /**
  * @brief Initialize LLVM globals and engines needed for JIT compilation
@@ -34,6 +36,11 @@ INIT static void llvm_init() {
   char *err;
   LLVMBool ret = LLVMCreateJITCompilerForModule(&ex_engine, module, 3, &err);
   xassert(ret);
+
+  builder = LLVMCreateBuilder();
+  xassert(builder != NULL);
+
+  llvm_u64 = LLVMInt64Type();
 }
 
 /**
@@ -46,4 +53,18 @@ DESTROY static void llvm_destroy() {
 }
 
 void llvm_munge(lfunc_t *func) {
+  LLVMBasicBlockRef blocks[func->num_instrs];
+  LLVMValueRef regs[func->max_stack];
+  LLVMValueRef consts[func->num_consts];
+  size_t i;
+
+  for (i = 0; i < func->num_instrs; i++) {
+    blocks[i] = LLVMGetInsertBlock(builder);
+  }
+  for (i = 0; i < func->max_stack; i++) {
+    regs[i] = LLVMBuildAlloca(builder, llvm_u64, "reg");
+  }
+  for (i = 0; i < func->num_consts; i++) {
+    consts[i] = LLVMConstInt(llvm_u64, func->consts[i], FALSE);
+  }
 }
