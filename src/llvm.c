84e59057fa9c533162302930093110b20986ce49
Alex Crichton <alex@alexcrichton.com>
2012-03-20 02:16:38 -0400

    Get us to the point where we can actually run "compiled" code!

--- src/llvm.c
+++ src/llvm.c
@@ -35,6 +35,7 @@ static LLVMValueRef lvc_data_mask;
  * @brief Initialize LLVM globals and engines needed for JIT compilation
  */
 void llvm_init() {
+  LLVMLinkInJIT();
   LLVMInitializeNativeTarget();
   module = LLVMModuleCreateWithName("joule");
   xassert(module != NULL);
@@ -49,7 +50,7 @@ void llvm_init() {
   /* Builder and execution engine */
   char *err;
   LLVMBool ret = LLVMCreateJITCompilerForModule(&ex_engine, module, 100, &err);
-  xassert(ret);
+  xassert(!ret);
   builder = LLVMCreateBuilder();
   xassert(builder != NULL);
 
@@ -84,9 +85,9 @@ void llvm_init() {
  * @brief Deallocates all memory associated with LLVM allocated on startup
  */
 DESTROY static void llvm_destroy() {
-  LLVMDisposeExecutionEngine(ex_engine);
   LLVMDisposePassManager(pass_manager);
-  LLVMDisposeModule(module);
+  LLVMDisposeBuilder(builder);
+  LLVMDisposeExecutionEngine(ex_engine);
 }
 
 void llvm_munge(lfunc_t *func) {
@@ -95,17 +96,15 @@ void llvm_munge(lfunc_t *func) {
   LLVMValueRef consts[func->num_consts];
   char name[20];
   u32 i, j;
-
-  LLVMTypeRef params[] = {
-    llvm_void_ptr,
-    LLVMPointerType(LLVMArrayType(llvm_u64, func->max_stack), 0),
-  };
+  LLVMValueRef indices[2] = {LLVMConstInt(llvm_u64, 0, 0), NULL};
+  LLVMTypeRef params[] = { llvm_void_ptr, llvm_u32 };
 
   LLVMTypeRef  funtyp   = LLVMFunctionType(LLVMVoidType(), params, 2, FALSE);
   LLVMValueRef function = LLVMAddFunction(module, "test", funtyp);
   LLVMValueRef closure  = LLVMGetParam(function, 0);
-  LLVMValueRef stack    = LLVMGetParam(function, 1);
+  LLVMValueRef stacki   = LLVMGetParam(function, 1);
 
+  /* Create the blocks and allocas */
   for (i = 0; i < func->num_instrs; i++) {
     blocks[i] = LLVMAppendBasicBlock(function, "block");
   }
@@ -118,14 +117,21 @@ void llvm_munge(lfunc_t *func) {
     regs[i] = LLVMBuildAlloca(builder, llvm_u64, name);
   }
 
+  /* Calculate closure->env */
   LLVMValueRef offset = LLVMConstInt(llvm_u64, offsetof(lclosure_t, env), 0);
   LLVMValueRef closure_addr = LLVMBuildPtrToInt(builder, closure, llvm_u64, "");
   LLVMValueRef env_addr = LLVMBuildAdd(builder, closure_addr, offset, "");
   env_addr = LLVMBuildIntToPtr(builder, env_addr, llvm_void_ptr_ptr, "");
   LLVMValueRef closure_env = LLVMBuildLoad(builder, env_addr, "env");
 
-  LLVMValueRef indices[2];
-  indices[0] = LLVMConstInt(llvm_u64, 0, 0);
+  /* Calculate stack base */
+  LLVMValueRef base_addr = LLVMConstInt(llvm_u64, (size_t) &vm_stack->base, 0);
+  LLVMTypeRef base_typ  = LLVMPointerType(LLVMPointerType(LLVMArrayType(llvm_u64, 2), 0), 0);
+  base_addr = LLVMBuildIntToPtr(builder, base_addr, base_typ, "");
+  LLVMValueRef stack = LLVMBuildLoad(builder, base_addr, "");
+  stack = LLVMBuildInBoundsGEP(builder, stack, &stacki, 1, "stack");
+
+  /* Copy the lua stack onto the C stack */
   for (i = 0; i < func->max_stack; i++) {
     indices[1] = LLVMConstInt(llvm_u64, i, 0);
     LLVMValueRef addr = LLVMBuildInBoundsGEP(builder, stack, indices, 2, "");
@@ -133,6 +139,7 @@ void llvm_munge(lfunc_t *func) {
     LLVMBuildStore(builder, val, regs[i]);
   }
 
+  /* Translate! */
   for (i = 0; i < func->num_instrs;) {
     LLVMPositionBuilderAtEnd(builder, blocks[i]);
     u32 code = func->instrs[i++];
@@ -156,14 +163,14 @@ void llvm_munge(lfunc_t *func) {
         /* TODO: assumes floats */
         LLVMValueRef bv, cv;
         if (B(code) >= 256) {
-          bv = LLVMBuildUIToFP(builder, consts[B(code) - 256],
+          bv = LLVMBuildBitCast(builder, consts[B(code) - 256],
                                llvm_double, "add_bf");
         } else {
           bv = LLVMBuildLoad(builder, regs[B(code)], "add_b64");
           bv = LLVMBuildBitCast(builder, bv, llvm_double, "add_bf");
         }
         if (C(code) >= 256) {
-          cv = LLVMBuildUIToFP(builder, consts[C(code) - 256],
+          cv = LLVMBuildBitCast(builder, consts[C(code) - 256],
                                llvm_double, "add_cf");
         } else {
           cv = LLVMBuildLoad(builder, regs[C(code)], "add_c64");
@@ -181,20 +188,20 @@ void llvm_munge(lfunc_t *func) {
         /* TODO: assumes floats */
         LLVMValueRef bv, cv;
         if (B(code) >= 256) {
-          bv = LLVMBuildUIToFP(builder, consts[B(code) - 256],
+          bv = LLVMBuildBitCast(builder, consts[B(code) - 256],
                                llvm_double, "lt_bf");
         } else {
           bv = LLVMBuildLoad(builder, regs[B(code)], "lt_b64");
           bv = LLVMBuildBitCast(builder, bv, llvm_double, "lt_bf");
         }
         if (C(code) >= 256) {
-          cv = LLVMBuildUIToFP(builder, consts[C(code) - 256],
+          cv = LLVMBuildBitCast(builder, consts[C(code) - 256],
                                llvm_double, "lt_cf");
         } else {
           cv = LLVMBuildLoad(builder, regs[C(code)], "lt_c64");
           cv = LLVMBuildBitCast(builder, cv, llvm_double, "lt_cf");
         }
-        LLVMRealPredicate pred = A(code) ? LLVMRealOGE : LLVMRealOLT;
+        LLVMRealPredicate pred = A(code) ? LLVMRealUGE : LLVMRealULT;
         LLVMValueRef res = LLVMBuildFCmp(builder, pred, bv, cv, "lt");
         LLVMBuildCondBr(builder, res, blocks[i + 1], blocks[i]);
         break;
@@ -218,7 +225,6 @@ void llvm_munge(lfunc_t *func) {
           closure_env,
           consts[BX(code)]
         };
-        LLVMDumpValue(fn);
         LLVMValueRef val = LLVMBuildCall(builder, fn, args, 2, "");
         LLVMBuildStore(builder, val, regs[A(code)]);
         LLVMBuildBr(builder, blocks[i]);
@@ -227,20 +233,23 @@ void llvm_munge(lfunc_t *func) {
 
       case OP_CALL: {
         /* TODO: varargs, multiple returns, etc... */
+        xassert(B(code) > 0 && C(code) > 0);
+        u32 num_args = B(code) - 1;
+        u32 num_rets = C(code) - 1;
         // copy things from c stack to lua stack
-        for (j = 0; j < func->max_stack; j++) {
+        u32 a = A(code);
+        for (j = a + 1; j < a + 1 + num_args; j++) {
           indices[1] = LLVMConstInt(llvm_u64, j, 0);
           LLVMValueRef addr = LLVMBuildInBoundsGEP(builder, stack, indices, 2, "");
           LLVMValueRef val  = LLVMBuildLoad(builder, regs[j], "");
           LLVMBuildStore(builder, val, addr);
         }
-        xassert(B(code) > 0 && C(code) > 0);
-        u32 num_args = B(code) - 1;
-        u32 num_rets = C(code) - 1;
+
         // get the function pointer
-        LLVMValueRef a_u64 = LLVMBuildPtrToInt(builder, regs[A(code)], llvm_u64, "");
-        LLVMValueRef cl_ptr = LLVMBuildAnd(builder, a_u64, lvc_data_mask, "call_and");
-        LLVMValueRef closure = LLVMBuildIntToPtr(builder, cl_ptr, llvm_void_ptr, "");
+        LLVMValueRef closure = LLVMBuildLoad(builder, regs[A(code)], "f");
+        closure = LLVMBuildAnd(builder, closure, lvc_data_mask, "");
+        closure = LLVMBuildIntToPtr(builder, closure, llvm_void_ptr, "");
+
         // call the function
         LLVMValueRef av = LLVMConstInt(llvm_u32, A(code), FALSE);
         LLVMValueRef fn = LLVMGetNamedFunction(module, "vm_fun");
@@ -249,9 +258,9 @@ void llvm_munge(lfunc_t *func) {
           closure,
           lvc_null,
           LLVMConstInt(llvm_u32, num_args, FALSE),
-          LLVMBuildAdd(builder, av, lvc_u32_one, "call_a_inc"),
+          LLVMBuildAdd(builder, stacki, LLVMConstAdd(av, lvc_u32_one), ""),
           LLVMConstInt(llvm_u32, num_rets, FALSE),
-          av
+          LLVMBuildAdd(builder, stacki, av, "")
         };
         /*LLVMValueRef ret =*/ LLVMBuildCall(builder, fn, args, 6, "");
         // nilify unused return parameters
@@ -262,8 +271,9 @@ void llvm_munge(lfunc_t *func) {
         };
         LLVMBuildCall(builder, memset_fn, memset_argvs, 3, "memset");
         */
+
         // copy things from lua stack back to c stack
-        for (j = 0; j < func->max_stack; j++) {
+        for (j = a; j < a + num_rets; j++) {
           indices[1] = LLVMConstInt(llvm_u64, j, 0);
           LLVMValueRef addr = LLVMBuildInBoundsGEP(builder, stack, indices, 2, "");
           LLVMValueRef val  = LLVMBuildLoad(builder, addr, "");
@@ -281,5 +291,16 @@ void llvm_munge(lfunc_t *func) {
   }
 
   LLVMRunFunctionPassManager(pass_manager, function);
-  LLVMDumpValue(function);
+  LLVMDumpModule(module);
+
+  u32 lstacki = vm_stack_alloc(vm_stack, 100);
+  static lclosure_t lclosure;
+  lclosure.type = LUAF_LUA;
+  lclosure.function.lua = func;
+  lclosure.env = lua_globals;
+  LLVMGenericValueRef args[2] = {
+    LLVMCreateGenericValueOfPointer(&lclosure),
+    LLVMCreateGenericValueOfInt(llvm_u32, lstacki, FALSE)
+  };
+  LLVMRunFunction(ex_engine, function, 2, args);
 }
