6e767ada334b39d7c1d0e177e51ecd926dcd4744
Robbie McElrath <rmcelrath@cmu.edu>
2012-03-25 17:05:29 -0400

    Increased run time of the while test

--- tests/while.lua
+++ tests/while.lua
@@ -1,11 +1,11 @@
 local i,j,k = 0,0,0
-while i < 10 do
+while i < 1000 do
   i = i+1
   j = 0
-  while j < 10 do
+  while j < 100 do
     j = j+1
     k = 0
-    while k < 10 do
+    while k < 100 do
       k = k+1
       if i+j+k == 7 then
         print(i,j,k)
