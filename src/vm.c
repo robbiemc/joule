3c69d69341a9caf531e8d0fd363b108adfcde4c8
Alex Crichton <alex@alexcrichton.com>
2012-03-29 16:54:49 -0400

    We want the MIN of argc/TRACEMAX, not the MAX...

--- src/vm.c
+++ src/vm.c
@@ -804,7 +804,7 @@ top:
           SETREG(a + i, LUAV_NIL);
         }
         closure->last_ret = a + i;
-        func->trace.instrs[pc][0] = (u8) MAX(argc, TRACEMAX);
+        func->trace.instrs[pc][0] = (u8) MIN(argc, TRACEMAX);
         for (i = 1; i < TRACELIMIT && i < limit + 1; i++) {
           SETTRACE(i, REG(a + i - 1));
         }
